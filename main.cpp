30251f27cf3802adbceb9114b0e9d1ff80e65235
Alinaneg <alinanegodneva@gmail.com>
2025-11-07 20:00:40 +0200

    second and third task with test mode

--- main.cpp
+++ main.cpp
@@ -10,21 +10,40 @@ void debug(const string& command) {
     cout << text << endl;
 }
 
+string trim(const string& str) {
+    size_t start = str.find_first_not_of(" \t\n\r");
+    size_t end = str.find_last_not_of(" \t\n\r");
+    if (start == string::npos || end == string::npos) {
+        return "";
+    }
+    return str.substr(start, end - start + 1);
+}
+
+
 int main() {
     string input;
+    string trinput = trim(input);
     
-    if (!testflag) {
-        cout << "$ "; 
-    }
-    
-    if (!getline(cin, input)) {
-        return 0;
-    }
-    
-    if (input.find("debug") == 0) {
-        debug(input);
-        if (testflag) return 0;
-    }
-    
+    while(true) {
+        if (!testflag) {
+            cout << "$ ";
+            cout.flush();
+        }
+        
+        if (!getline(cin, input)) {
+            break;
+        }
+        
+        if (input.find("debug") == 0) {
+            debug(input);
+            if (testflag) break;
+        }
+        
+        if (trinput.empty()) {
+            continue;
+        }
+        if (trinput == "\\q") {
+             break;
+        }
     return 0;
 }
